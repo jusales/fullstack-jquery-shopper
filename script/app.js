6296ec284ec1364162136d2dbbf42f3294e67713
Juliana Sales <jsales@seas.upenn.edu>
2018-06-29 08:22:41 -0400

    functionality of button still not working

--- script/app.js
+++ script/app.js
@@ -2,7 +2,15 @@ $(document).ready (function() {
     $('.announcement').append("<button id='button'>X</button>");
 
     var buttons = document.querySelectorAll("button");
-    buttons.addEventListener('click', function() {
-        $('.announcement').hide();
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            if(button.text === "X") {
+                $('.announcement').addClass('hidden');
+                $(button).addClass('clicked');
+            } else {
+                $('.announcement').removeClass('hidden');
+                $(button).removeClass('clicked');
+            }
+        })
     });
 });
